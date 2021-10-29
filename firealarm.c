836e22ae1f25db999837afa7f09cfa9bfddad480
dtrum1 <91457860+dtrum1@users.noreply.github.com>
2021-10-29 23:38:41 +1000

    Update firealarm.c
    using online compiler because VM is slow as balls, if you want to test you will have to uncomment all pthread and shm stuff

--- firealarm.c
+++ firealarm.c
@@ -1,35 +1,57 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <semaphore.h> 
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <unistd.h>
 
+//#include "carQueue.h"
+//#include "sharedMemoryOperations.h"
 
+#define SHARE_NAME "PARKING"
 #define LOWER 30
 #define UPPER 40
 #define LEVELS 5
-#define ARSIZE 30
-
+#define ARSIZE 8
 
 //HELPER FUNCTIONS
 int16_t tempGen(int lower, int upper);
-int usleep(int arg);
-double smoothedData(double arr[][ARSIZE], int i);
+double smoothedData(double arr[][ARSIZE], int i, double arg[4]);
 void fixedTemp(double arr[][ARSIZE]);
-
 
 //GLOBAL
 int ALARM = 0;
+int SWITCH = 0;
+//shared_memory_t shm;
 
 //MAIN
 int main()
 {
+    //shared_memory_t shm;
     //declare data
+    //1st row has raw data
+    //2nd row has smoothed data
     //init all to 0
-	double rawL1[2][ARSIZE] = {0};
-	double rawL2[2][ARSIZE] = {0};
-	double rawL3[2][ARSIZE] = {0};
-	double rawL4[2][ARSIZE] = {0};
-	double rawL5[2][ARSIZE] = {0};
+	double dataL1[2][ARSIZE] = {0};
+	double dataL2[2][ARSIZE] = {0};
+	double dataL3[2][ARSIZE] = {0};
+	double dataL4[2][ARSIZE] = {0};
+	double dataL5[2][ARSIZE] = {0};
 
     //finite loop for testing
     int i = 0;
@@ -45,10 +67,13 @@ int main()
             
         //check if possible to smooth data
         //need more than 5 entries
-        if (i > 4)
+        if (i > 4 || SWITCH == 1)
         {
-            double L1 = smoothedData(rawL1, i % ARSIZE);
-            rawL1[1][i] = L1;
+            //get previous array date
+            //last 4 elements for next set of data from smoothedData
+            double prevData[4] = {rawL1[1][26],rawL1[1][27], rawL1[1][28], rawL1[1][29]}; 
+            double L1 = smoothedData(rawL1, i % ARSIZE, prevData);
+            rawL1[1][i % 30] = L1;
 
             //test for fire
             fixedTemp(rawL1);           
@@ -56,6 +81,7 @@ int main()
             //fixedTemp(rawL3);
             //fixedTemp(rawL4);
             //fixedTemp(rawL5);
+            SWITCH = 1;
             
         }
 
@@ -81,11 +107,37 @@ int main()
         printf("%d. %f\n",i+1,rawL1[1][i]);
     }
     
-    //OPEN ALL GATES
-    if (ALARM == 1)
+    //open all gates
+    for (int i = 0; i < LEVELS; i++)
     {
-        printf("ALARM TRIPPED");
+     /*   //open exit
+        pthread_mutex_lock(&shm.data->exit[i].gate.gatemutex);
+        shm.data->exit[i].gate.status = 'O';
+        
+        //open entrance
+        pthread_mutex_lock(&shm.data->entrance[i].gate.gatemutex);
+        shm.data->entrance[i].gate.status = 'O';
+    */
+        printf("Entrance and Exit %d Open\n", i + 1);
     }
+    
+    //print evacuate on all signs
+    char evacuate[] = {"EVACUATE"};
+    int size = sizeof(evacuate) / sizeof(evacuate[0]);
+    
+    while(1)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < LEVELS; j++)
+            {
+                //shm.data->entrance[i].informationSign.display = evacuate[j]; 
+            }
+        usleep(20000);
+        }
+    }
+    
+    
     return 0;
 }
 
@@ -96,13 +148,34 @@ int16_t tempGen(int lower, int upper)
 	return num;    
 }
 
-double smoothedData(double arr[][ARSIZE], int index)
+double smoothedData(double arr[][ARSIZE], int index, double prevData[4])
 {
-    //sum the previous 5 values of array
+    //see if index is less than 1 
+    //from loop around
     int sum = 0;
-    for (int i = index; index - 5 < i ; i--)
+    if (index < 5)
     {
-        sum += arr[0][i];
+        //see if index is less than 0 from second time around
+        //can use prev data to find average for first 4 values
+        //sum values from new array
+        for (int i = 0; index < i ; i++)
+        {
+            sum += arr[0][i];
+        }
+        
+        //sum values from old array
+        for (int i = 30 - (5 - index); i < 30; i++)
+        {
+            sum += prevData[i];
+        }
+    }
+    else
+    {
+        //sum the previous 5 values of array
+        for (int i = index; index - 5 < i ; i--)
+        {
+            sum += arr[0][i];
+        }
     }
     
     //average the values
@@ -133,26 +206,7 @@ void fixedTemp(double arr[][ARSIZE])
 	}	
 }	
 
-// Logic for the fire alarm:
-//int difference = 0;
-//void RateofRise(int arr[][]){
-//	if(difference >= 8){
-//		ALARM = 1;
-//	}	
-//}
 
-// This function 
-// if the most recent temperature is 8°C (or more) hotter than the 30th most recent temperature,
-// the temperature is considered to be growing at a fast enough rate that there must be a fire.
-void RateofRise(double arr[][30]){
-    int i = 0;
-    for (i = 0; i < 30; i++){
-        if ( abs ( arr[0][i] - arr[0][i+1]) >= 8){
-            ALARM = 1;;
-        }
-    }
-
-}
 
 
 
